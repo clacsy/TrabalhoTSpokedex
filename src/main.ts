5db173c753636f3374f7fed58517660d9acbf776
PortellaAlly <allysonpalmeirim12@gmail.com>
2025-07-23 08:42:42 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Pokemon } from './app/pokemon/pokemon';
 
 @Component({
   selector: 'app-root',
+  imports: [Pokemon],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <app-pokemon></app-pokemon>
   `,
 })
 export class App {
